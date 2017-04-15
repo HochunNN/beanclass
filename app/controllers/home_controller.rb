e477f5c0ff6c252f30d58d8b0dcee497d56c83bf
hochunnn <hochun0122@naver.com>
2017-04-15 18:19:37 +0000

    검색결과 수정!

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -23,17 +23,23 @@ class HomeController < ApplicationController
       
       # 교실 사용 불가능 한 곳들 3가지 경우
       # 1. 수업의 시작시가 원하는 시간보다 더 빠르지만 끝시는 걸쳐있는경우
-      a = classroom_day_filtered.where("class_begin < ?", time_begin_wanted).where("class_end > ? AND class_end <= ? ", time_begin_wanted, time_end_wanted)
-      # 2. 수업의 시작시와 끝시가 둘다 원하는 시간 안에 있는 경우
-      b = classroom_day_filtered.where("class_begin >= ? AND class_end <= ? ", time_begin_wanted, time_end_wanted)
-      # 3. 수업의 끝시는 원하는 시간보다 더 느리지만 시작시가 걸쳐있는경우.
-      c = classroom_day_filtered.where("class_end > ?", time_end_wanted).where("class_begin >= ? AND class_begin <= ? ", time_begin_wanted, time_end_wanted)
+      a = classroom_day_filtered.where("class_begin <= ?", time_begin_wanted).where("class_end > ? AND class_end <= ? ", time_begin_wanted, time_end_wanted)
+      # 2. 수업의 시작시가 원하는 시간보다 더 빠르지만 끝시는 나가있는경우
+      b = classroom_day_filtered.where("class_begin <= ?  AND class_end >= ? ", time_begin_wanted, time_end_wanted)
+      # 3. 수업의 시작시가 원하는 시간보다 후, 끝시가 원하는 끝시보다 안
+      c = classroom_day_filtered.where("class_begin >= ? AND class_begin < ? ", time_begin_wanted, time_end_wanted).where("class_end <= ?", time_end_wanted)
+      # 4. 수업의 시작시가 원하는 시간보다 후, 끝시는 원하는 시간보다 더 느림
+      d = classroom_day_filtered.where("class_end >= ?", time_end_wanted).where("class_begin >= ? AND class_begin < ? ", time_begin_wanted, time_end_wanted)
       
-      d = a.pluck(:class_room) + b.pluck(:class_room) + c.pluck(:class_room)
-      e = d.uniq
-      classrooms = classroom_building_filter.pluck(:class_room).uniq
+      e = a.pluck(:class_room) + b.pluck(:class_room) + c.pluck(:class_room) + d.pluck(:class_room)
+      @aa = a.pluck(:class_room)
+      @bb = b.pluck(:class_room)
+      @cc = c.pluck(:class_room)
+      @dd = d.pluck(:class_room)
+      @f = e.uniq
+      @classrooms = classroom_building_filter.pluck(:class_room).uniq
       
-      @final_result = classrooms - e
+      @final_result = @classrooms - @f
       @time_begin_wanted = params[:begin]
       @time_end_wanted = params[:end]
       unless @final_result.present?
